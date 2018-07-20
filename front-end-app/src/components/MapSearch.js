31b47a8bb51e26e4f8758abf888cab7761c81952
Victor Nhem <nhemvictor@gmail.com>
2018-07-19 21:52:18 -0500

    Add MapSearch component file

---
+++ front-end-app/src/components/MapSearch.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class MapSearch extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Physician Location</h1>
+        </header>
+        <p className="App-intro">
+          Find your physician's location by filling the name below:
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MapSearch;
