3150178af04c582e8e5941e097dc26852280b687
Victor Nhem <nhemvictor@gmail.com>
2018-07-19 19:06:09 -0500

    Add server GET /search

--- back-end-app/server/models/contact.js
+++ back-end-app/server/models/contact.js
@@ -14,7 +14,11 @@ const Contact = mongoose.model('Contact', {
     type: String,
     required: true,
     minlength: 1
-  }
+  },
+  Recipient_Primary_Business_Street_Address_Line1: {},
+  Recipient_City: {},
+  Recipient_State: {},
+  Recipient_Zip_Code: {}
 });
 
 module.exports = {Contact};
--- back-end-app/server/server.js
+++ back-end-app/server/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
 const {Contact} = require('./models/contact');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.get('/search', (req, res) => {
+  let nameFirst = new RegExp('alan', 'i');
+  let nameLast = new RegExp('', 'i');
+  let nameMiddle = new RegExp('', 'i');
+  return Contact.find({
+    Physician_First_Name: nameFirst,
+    Physician_Middle_Name: nameMiddle,
+    Physician_Last_Name: nameLast}, (err, results) => {
+    return res.send(results.map((result) => {
+      return {
+        firstName: result.Physician_First_Name,
+        middleName: result.Physician_Middle_Name,
+        lastName: result.Physician_Last_Name,
+        street: result.Recipient_Primary_Business_Street_Address_Line1,
+        city: result.Recipient_City,
+        state: result.Recipient_State,
+        zipCode: result.Recipient_Zip_Code
+      }
+    }));
+  });
+});
 
 app.listen(3000, () => {
   console.log('port 3000 starts')
