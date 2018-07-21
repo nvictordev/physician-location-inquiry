5c7b46293ce81ce9c8fc2b195d8426c77163d124
Victor Nhem <nhemvictor@gmail.com>
2018-07-20 23:30:03 -0500

    Add api getNames method

--- front-end-app/src/components/MapSearch.js
+++ front-end-app/src/components/MapSearch.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class MapSearch extends Component {
+  state = {
+    firstNameQuery: '', 
+    names: [],
+    errorMessage: false
+  }
+  componentDidMount() {
+    this.getNames();
+  }
+  getNames = () => {
+    axios.get(`http://localhost:5001/search`)
+      .then(res => res.data)
+      .then((contacts) => {
+        this.setState({
+          names: contacts.map((contact) => {
+            return contact.lastName;
+          })
+        })
+      })
+  }
+
   render() {
+    const { firstNameQuery, names, errorMessage } = this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -10,6 +32,18 @@ class MapSearch extends Component {
         <p className="App-intro">
           Find your physician's location by filling the name below:
         </p>
+        <div>
+          <form action='http://localhost:5001/search' methond='GET'>
+            <input type="text"
+              name="search"
+              placeholder="First Name"
+              value={firstNameQuery}
+              onChange={e => this.updateSearch(e.target.value)}/>
+          </form>
+        </div>
+        <div>
+          {names}
+        </div>
       </div>
     );
   }
