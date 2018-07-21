5a1eaa43e5a28b03c28d6da1ab1bb706b6d1ea93
Victor Nhem <nhemvictor@gmail.com>
2018-07-21 15:23:36 -0500

    Implement regex in req.query

--- back-end-app/server/server.js
+++ back-end-app/server/server.js
@@ -10,22 +10,25 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/search', (req, res) => {
-  let nameFirst = new RegExp('alan', 'i');
-  let nameLast = new RegExp('', 'i');
-  let nameMiddle = new RegExp('', 'i');
+  let nameFirst = { $regex: `^${req.query.firstName}`, $options: 'i' };
+  let nameMiddle = { $regex: `^${req.query.middleName}`, $options: 'i' };
+  let nameLast = { $regex: `^${req.query.lastName}`, $options: 'i' };
   return Contact.find({
     Physician_First_Name: nameFirst,
     Physician_Middle_Name: nameMiddle,
     Physician_Last_Name: nameLast}, (err, results) => {
     return res.send(results.map((result) => {
+      let firstName = result.Physician_First_Name;
+      let middleName = result.Physician_Middle_Name;
+      let lastName = result.Physician_Last_Name;
       return {
-        firstName: result.Physician_First_Name,
-        middleName: result.Physician_Middle_Name,
-        lastName: result.Physician_Last_Name,
+        firstName,
+        middleName,
+        lastName,
         street: result.Recipient_Primary_Business_Street_Address_Line1,
         city: result.Recipient_City,
         state: result.Recipient_State,
-        zipCode: result.Recipient_Zip_Code
+        zipCode: result.Recipient_Zip_Code,
       }
     }));
   });
