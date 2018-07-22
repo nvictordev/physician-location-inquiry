aadfd3140fb7dbd88b80639234762b89ca02f293
Victor Nhem <nhemvictor@gmail.com>
2018-07-22 13:08:12 -0500

    Add server GET /address

--- back-end-app/server/server.js
+++ back-end-app/server/server.js
@@ -28,11 +28,30 @@ app.get('/search', (req, res) => {
         street: result.Recipient_Primary_Business_Street_Address_Line1,
         city: result.Recipient_City,
         state: result.Recipient_State,
-        zipCode: result.Recipient_Zip_Code,
+        zipCode: result.Recipient_Zip_Code
       }
     }));
   });
 });
+
+app.get('/address', (req, res) => {
+  let nameFirst = new RegExp(req.query.firstName, 'i');
+  let nameMiddle = new RegExp(req.query.middleName, 'i');
+  let nameLast = new RegExp(req.query.lastName, 'i');
+  return Contact.findOne({
+    Physician_First_Name: nameFirst,
+    Physician_Middle_Name: nameMiddle,
+    Physician_Last_Name: nameLast
+  }, (err, result) => {
+    return res.send({
+      street: result.Recipient_Primary_Business_Street_Address_Line1,
+      city: result. Recipient_City,
+      state: result.Recipient_State,
+      zipCode: result.Recipient_Zip_Code
+    });
+  });
+});
+
 app.use((req, res, next) => {
   res.status(404).send('<h2 align=center> Page Not Found!</h2>');
 });
