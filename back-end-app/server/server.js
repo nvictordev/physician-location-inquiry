da3e9cd4c8ec9aa25c70ed92b8460d8405fb50b2
Victor Nhem <nhemvictor@gmail.com>
2018-07-23 23:15:12 -0500

    Add error handling to both GET methods

--- back-end-app/server/server.js
+++ back-end-app/server/server.js
@@ -9,6 +9,15 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
+const titleCase = (str) => {
+  lowerStr = str.toLowerCase().split('');
+  for (var i = 0; i < lowerStr.length; i++) {
+    lowerStr[0] = lowerStr[0].toUpperCase();
+    word = lowerStr.join('');
+  }
+  return word;
+};
+
 app.get('/search', (req, res) => {
   let nameFirst = { $regex: `^${req.query.firstName}`, $options: 'i' };
   let nameMiddle = { $regex: `^${req.query.middleName}`, $options: 'i' };
@@ -17,23 +26,22 @@ app.get('/search', (req, res) => {
     Physician_First_Name: nameFirst,
     Physician_Middle_Name: nameMiddle,
     Physician_Last_Name: nameLast}, (err, results) => {
-    return res.send(results.map((result) => {
-      let firstName = result.Physician_First_Name;
-      let middleName = result.Physician_Middle_Name;
-      let lastName = result.Physician_Last_Name;
-      return {
-        firstName,
-        middleName,
-        lastName,
-        street: result.Recipient_Primary_Business_Street_Address_Line1,
-        city: result.Recipient_City,
-        state: result.Recipient_State,
-        zipCode: result.Recipient_Zip_Code
+      if (results) {
+        return res.send(results.map((result) => {
+          let firstName = titleCase(result.Physician_First_Name);
+          let middleName = titleCase(result.Physician_Middle_Name);
+          let lastName = titleCase(result.Physician_Last_Name);
+          return {
+            firstName,
+            middleName,
+            lastName
+          }
+        }));
+      } else {
+        return res.status(404).send(err);
       }
-    }));
   });
 });
-
 app.get('/address', (req, res) => {
   let nameFirst = new RegExp(req.query.firstName, 'i');
   let nameMiddle = new RegExp(req.query.middleName, 'i');
@@ -43,12 +51,16 @@ app.get('/address', (req, res) => {
     Physician_Middle_Name: nameMiddle,
     Physician_Last_Name: nameLast
   }, (err, result) => {
-    return res.send({
-      street: result.Recipient_Primary_Business_Street_Address_Line1,
-      city: result. Recipient_City,
-      state: result.Recipient_State,
-      zipCode: result.Recipient_Zip_Code
-    });
+    if (results) {
+      return res.send({
+        street: result.Recipient_Primary_Business_Street_Address_Line1,
+        city: result. Recipient_City,
+        state: result.Recipient_State,
+        zipCode: result.Recipient_Zip_Code
+      });
+    } else if (err) {
+      return res.status(404).send(err);
+    }
   });
 });
 
